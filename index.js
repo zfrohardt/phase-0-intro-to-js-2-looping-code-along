70ce8676b15183c00b35d35a7e1a55e3b9c01c1e
Zach Frohardt <zfrohardt@gmail.com>
2021-03-25 08:53:30 -0700

    Assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+function writeCards(names, occasion) {
+    let cards = [];
+    for(let i = 0; i < names.length; i++) {
+        cards.push(`Thank you, ${names[i]}, for the wonderful ${occasion} gift!`);
+    }
+    return cards;
+}
+
+function countDown(n) {
+    for(; n >= 0; n--) {
+        console.log(n);
+    }
+}
